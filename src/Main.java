69944b66f667327886c8e2918c8fbbb5c1797c26
dmitriykorotkiy <korotkiydimitriy@gmail.com>
2017-05-30 21:02:04 +0300

    new changes

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,62 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.logging.Logger;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         List<String> listBET = new ArrayList<>();
 
-        File input = new File("C:\\1.html");
-        Document document = Jsoup.parse(input,"UTF-8");
+//       File input = new File("C:\\1.html");
+//       Document document = Jsoup.parse(input,"UTF-8");
+//
+//        FileWriter writer = new FileWriter("C:\\bets.txt",false);
+//
+//        Elements positionElements = document.getElementsByAttributeValue("class","style4");
+//
+//        for (Element betElement : positionElements) {            // кладу текст в коллекцию
+//             listBET.add(betElement.text());
+//      }
+//
+//       for (int i = 0; i < positionElements.size(); i++) {
+//           String lineSeparator = System.getProperty("line.separator");
+//           String text = listBET.get(i) + lineSeparator;
+//           writer.write(text);
+//           writer.flush();
+//       }
 
+
+        System.setProperty("webdriver.chrome.driver", "C:\\projects\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("C:\\1.html");
+
+
+        List<WebElement> allPositions = driver.findElements(By.className("style4"));
         FileWriter writer = new FileWriter("C:\\bets.txt",false);
 
-        Elements positionElements = document.getElementsByAttributeValue("class","style4");
-
-         for (Element betElement : positionElements) {            // кладу текст в коллекцию
-             listBET.add(betElement.text());
-       }
-
-        for (int i = 0; i < positionElements.size(); i++) {
+        for (WebElement element : allPositions) {
             String lineSeparator = System.getProperty("line.separator");
-            String text = listBET.get(i) + lineSeparator;
-            writer.write(text);
+            String x = element.getText() + lineSeparator;
+            writer.write(x);
             writer.flush();
         }
+
+        driver.quit();
 
     }
 
